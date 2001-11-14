485e08fba4a5549943490ee1fb101052d453e16c
bitman <bitman>
2001-11-14 03:42:33 +0000

    Added new panels (oops forgot)

---
+++ panel_dd.h
@@ -0,0 +1,7 @@
+/* TheDraw C Screen Image.  Date: 11/13/01 */
+/* $Id: panel_dd.h,v 1.1 2001/11/14 03:42:33 bitman Exp $ */
+
+#define PANEL_DIRDIALOG_WIDTH 20
+#define PANEL_DIRDIALOG_DEPTH 22
+#define PANEL_DIRDIALOG_LENGTH 880
+extern unsigned char PANEL_DIRDIALOG [];
---
+++ panel_fn.h
@@ -0,0 +1,7 @@
+/* TheDraw C Screen Image.  Date: 11/13/01 */
+/* $Id: panel_fn.h,v 1.1 2001/11/14 03:42:33 bitman Exp $ */
+
+#define PANEL_FILENAME_WIDTH 20
+#define PANEL_FILENAME_DEPTH 22
+#define PANEL_FILENAME_LENGTH 880
+extern unsigned char PANEL_FILENAME [];
