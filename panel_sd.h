273de625bc5b5efb38833515b50de82f71d540bc
bitman <bitman>
2002-05-04 04:19:16 +0000

    Panels added for tile info and stats dialog

---
+++ panel_sd.h
@@ -0,0 +1,6 @@
+/* TheDraw C Screen Image.  Date: 05/03/02 */
+
+#define PANEL_STATS_DIALOG_WIDTH 20
+#define PANEL_STATS_DIALOG_DEPTH 22
+#define PANEL_STATS_DIALOG_LENGTH 880
+extern unsigned char PANEL_STATS_DIALOG [];
---
+++ panel_ti.h
@@ -0,0 +1,5 @@
+/* TheDraw C Screen Image.  Date: 05/03/02 */
+#define PANEL_TILE_INFO_WIDTH 20
+#define PANEL_TILE_INFO_DEPTH 22
+#define PANEL_TILE_INFO_LENGTH 880
+extern unsigned char PANEL_TILE_INFO [];
