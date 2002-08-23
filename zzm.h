e9f467f0ab5e6a988f717640121d158cca329383
bitman <bitman>
2002-08-23 22:34:49 +0000

    Removed playback code from zzm.c/h

--- zzm.h
+++ zzm.h
@@ -1,5 +1,5 @@
 /* zzm.h  -- zzm file routines
- * $Id: zzm.h,v 1.7 2002/08/23 21:34:12 bitman Exp $
+ * $Id: zzm.h,v 1.8 2002/08/23 22:34:49 bitman Exp $
  * Copyright (C) 2000 Ryan Phillips <bitman@scn.org>
  *
  * This program is free software; you can redistribute it and/or modify
@@ -23,49 +23,11 @@
 #include "svector.h"
 #include "display.h"
 
-#if 0
-typedef struct zzmplaystate {
-	int octave;     /* Current octave */
-	int duration;   /* Current note duration */
-	int pos;        /* Position on zzm line */
-	int slur;       /* Are we slurring presently? */
-} zzmplaystate;
-
-/* ZZM note types */
-#define ZZM_ERROR 0   /* Usually end-of-string */
-#define ZZM_NOTE  1   /* Just a note */
-#define ZZM_REST  2   /* Rest for a moment */
-#define ZZM_DRUM  3   /* Tick-tock */
-
-/* ZZM octave bounds */
-#define ZZM_MAXOCTAVE 6
-#define ZZM_MINOCTAVE 1
-#endif
 
 /* zzmpullsong() - pulls song #songnum out of zzmv and returns it */
 stringvector zzmpullsong(stringvector * zzmv, int songnum);
 
 /* zzmpicksong() - presents a dialog to choose a song based on title */
 int zzmpicksong(stringvector * zzmv, displaymethod * d);
-
-#if 0
-/* resetzzmplaystate() - clears state to default settings */
-void resetzzmplaystate(zzmplaystate * s);
-
-/* zzmplaynote() - plays a single note from a tune */
-zzmnote zzmgetnote(char * tune, zzmplaystate * s);
-
-/* zzmgetfrequency() - get the frequency of a zzmnote */
-int zzmgetfrequency(zzmnote note);
-
-/* zzmOpenaudio() - open the audio device */
-int zzmOpenaudio();
-
-/* zzmPlaynote() - play a note to the audio device */
-void zzmPlaynote(zzmnote note);
-
-/* zzmCloseaudio() - close the audio device */
-void zzmCloseaudio();
-#endif
 
 #endif
