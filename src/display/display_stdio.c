9dfb3bde2f1367a9748601037fee4e1405b0b870
bitman <bitman>
2003-11-02 22:21:53 +0000

    Added :print command to print buffered screen data

--- src/display/display_stdio.c
+++ src/display/display_stdio.c
@@ -1,5 +1,5 @@
 /* display_stdio.c   -- Dummy display using stdio
- * $Id: display_stdio.c,v 1.1 2003/11/01 23:45:56 bitman Exp $
+ * $Id: display_stdio.c,v 1.2 2003/11/02 22:21:53 bitman Exp $
  * Copyright (C) 2003 Ryan Phillips <bitman@users.sourceforge.net>
  *
  * This program is free software; you can redistribute it and/or modify
@@ -19,6 +19,8 @@
 
 #include "display_stdio.h"
 
+#include "textblock.h"
+
 #include <stdio.h>
 #include <string.h>
 
@@ -26,30 +28,29 @@
 
 static int shift_state = 0;
 static int brief = 0;
+static int cursorx = 0;
+static int cursory = 0;
 
-void display_stdio_print_help(void)
-{
-	/* A helpful message */
-	char message[] =
-		"This display driver is for testing purposes only.\n\n"
-		"To provide input, simply type a string of characters and press ENTER.\n"
-		"For special characters, use one of the following special commands:\n"
-		"\t# number   uses the decimal value of number\n"
-		"\tx hex      uses the hexadecimal value of hex\n"
-		"\t\\symbol    escapes the given symbol (including newline!)\n"
-		"\t: command  use a key name (e.g. up, left, esc, enter) or\n"
-		"\t           one of (shift, SHIFT, brief, verbose).\n\n"
-		;
+static textBlock * textbuffer = NULL;
 
-	printf(message);
-}
+void display_stdio_print_help(void);
+void display_stdio_print_buffer(void);
+
 
 int display_stdio_init(void)
 {
 	shift_state = 0;
+
+	/* Clear the text buffer if necessary */
+	if (textbuffer != NULL) {
+		deleteTextBlock(textbuffer);
+	}
+
+	textbuffer = createTextBlock(80, 25);
+
 	printf("%s version %s initialized\n", display_stdio.name, display_stdio.version);
 	display_stdio_print_help();
-	printf("Enter a command or press enter to continue...\n");
+	printf("Enter a command or press a key and enter to continue...\n");
 	display_stdio_getch();
 
 	return 1;
@@ -57,12 +58,18 @@ int display_stdio_init(void)
 
 void display_stdio_end(void)
 {
+	deleteTextBlock(textbuffer);
+	textbuffer = NULL;
+
 	printf("%s closed.\n\n", display_stdio.name);
 }
 
 void display_stdio_putch(int x, int y, int ch, int co)
 {
 	char show_char = '?';
+
+	/* Draw to the text buffer */
+	textBlockPutch(textbuffer, x, y, ch, co);
 
 	if (ch >= ' ' && ch <= '~')
 		show_char = ch;
@@ -99,15 +106,18 @@ int display_stdio_getch(void)
 		if (!strcmp(command, "enter"))   return DKEY_ENTER;
 
 		/* Set shift state */
-		if (!strcmp(command, "shift")) { shift_state = 0; return 0; }
-		if (!strcmp(command, "SHIFT")) { shift_state = 1; return 0; }
+		if (!strcmp(command, "shift")) { shift_state = 0; return display_stdio_getch(); }
+		if (!strcmp(command, "SHIFT")) { shift_state = 1; return display_stdio_getch(); }
 
 		/* Brief vs. verbose display */
-		if (!strcmp(command, "brief"))   { brief = 1; return 0; }
-		if (!strcmp(command, "verbose")) { brief = 0; return 0; }
+		if (!strcmp(command, "brief"))   { brief = 1; return display_stdio_getch(); }
+		if (!strcmp(command, "verbose")) { brief = 0; return display_stdio_getch(); }
+
+		/* Print the buffer */
+		if (!strcmp(command, "print"))   { display_stdio_print_buffer(); return display_stdio_getch(); }
 
 		/* Help */
-		if (!strcmp(command, "help")) { display_stdio_print_help(); return 0; }
+		if (!strcmp(command, "help")) { display_stdio_print_help(); return display_stdio_getch(); }
 
 		/* Return first character */
 		return command[0];
@@ -136,10 +146,20 @@ void display_stdio_cursorgo(int x, int y)
 		printf("\n[-> (%02X, %02X)] ", x, y);
 	else
 		printf("cursor moved to\t(%02X, %02X)\n", x, y);
+
+	cursorx = x;
+	cursory = y;
 }
 
 void display_stdio_print(int x, int y, int c, char *s)
 {
+	int i;
+
+	/* Draw to the text buffer */
+	for (i = 0; s[i] != '\x0' && (x + i) < textbuffer->width; i++) {
+		textBlockPutch(textbuffer, x + i, y, s[i], c);
+	}
+
 	if (brief)
 		printf("\n[(%02X, %02X) %02X] %s\n", x, y, c, s);
 	else
@@ -182,6 +202,50 @@ void display_stdio_update(int x, int y, int w, int h)
 	else
 		printf("updating area from\t(%02X, %02X) of size (%02Xx%02X)\n", x, y, w, h);
 }
+
+
+/* Helper functions */
+
+void display_stdio_print_help(void)
+{
+	/* A helpful message */
+	char message[] =
+		"This display driver is for testing purposes only.\n\n"
+		"To provide input, simply type a string of characters and press ENTER.\n"
+		"For special characters, use one of the following special commands:\n"
+		"\t# number   uses the decimal value of number\n"
+		"\tx hex      uses the hexadecimal value of hex\n"
+		"\t\\symbol    escapes the given symbol (including newline!)\n"
+		"\t: command  use a key name (e.g. up, left, esc, enter) or\n"
+		"\t           one of (shift, SHIFT, brief, verbose, print).\n\n"
+		;
+
+	printf(message);
+}
+
+void display_stdio_print_buffer(void)
+{
+	int x, y;
+
+	for (y = 0; y < textbuffer->height; y++) {
+		for (x = 0; x < textbuffer->width; x++) {
+			char ch = textBlockChar(textbuffer, x, y);
+			/* Nulls become spaces */
+			if (ch == '\x0')
+				ch = ' ';
+
+			/* Special characters become '?' */
+			if (ch < ' ' || ch > '~')
+				ch = '?';
+
+			if (x == cursorx && y == cursory)
+				ch = '*';
+
+			printf("%c", ch);
+		}
+	}
+}
+
 
 displaymethod display_stdio = {
 	NULL,
